3d706f612081e580e454523834dcc68130d51123
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-10-06 18:28:24 +0200

    proper rendering of chat screen

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -2,8 +2,9 @@ module View exposing (view)
 
 -- import Listings
 
-import Html exposing (Html, button, div, footer, header, span, text)
-import Html.Attributes exposing (class)
+import Dict
+import Html exposing (Html, button, div, dl, footer, header, input, span, text)
+import Html.Attributes exposing (attribute, class, maxlength, name, placeholder, title)
 import Html.Events exposing (onClick)
 import Models exposing (Model)
 import Msgs exposing (Msg)
@@ -14,7 +15,76 @@ view model =
     div []
         [ text "This is where the magic happens!"
         , button [ onClick (Msgs.SendMessage "This is a message") ] [ text "Send!" ]
-        , text ("Username: " ++ (toString model.current_user_name))
+        , text ("Username: " ++ toString model.current_user_name)
         , text "Messages: "
-        , div [] [ text (toString model.messages) ]
+
+        -- , div [] [ text (toString model.messages) ]
+        , container model
+        ]
+
+
+data name val =
+    Html.Attributes.attribute ("data-" ++ name) val
+
+
+container model =
+    div [ class "planga--chat-container" ]
+        [ messages model
+        , newMessageForm model
+        ]
+
+
+messages model =
+    let
+        message_list =
+            model.messages
+                |> Dict.values
+
+        messages_html =
+            message_list
+                |> List.map (message model.current_user_name)
+    in
+    dl [ class "planga--chat-messages" ]
+        messages_html
+
+
+message current_user_name message =
+    let
+        message_class =
+            case current_user_name |> Maybe.map (\val -> val == message.name) of
+                Just True ->
+                    "planga--chat-message planga--chat-current-user-message"
+                _ ->
+                    "planga--chat-message"
+    in
+
+      div
+          [ class message_class
+          , data "message-sent-at" message.sent_at
+          , data "message-uuid" message.uuid
+          ]
+          [ div [ class "planga--chat-message-sent-at-wrapper" ]
+              [ span
+                  [ class "planga--chat-message-sent-at"
+                  , title message.sent_at
+                  ]
+                  [ text message.sent_at
+                  ]
+              ]
+          , div [ class "planga--chat-author-wrapper" ]
+              [ span [ class "planga--chat-author-name" ] [ text message.name ]
+              , span [ class "planga--chat-message-separator" ] [ text ":   " ]
+              ]
+          , div [ class "planga--chat-message-content" ] [ text message.content ]
+          ]
+
+
+newMessageForm model =
+    div [ class "planga--new-message-form" ]
+        [ div [ class "planga--new-message-field-wrapper" ]
+            [ input [ maxlength 4096, placeholder "PLACEHOLDER TODO", name "planga-new-message-field", class "planga--new-message-field" ] []
+            ]
+        , button [ class "planga--new-message-submit-button" ]
+            [ text "Send"
+            ]
         ]
